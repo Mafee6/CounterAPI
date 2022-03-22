92cb5a30325345ce1ec7bb12a77c8ad1ef0cda63
Mafee7 <64575804+Mafee6@users.noreply.github.com>
2022-03-22 13:48:53 +0530

    docs content, other logs

--- website/docs/index.js
+++ website/docs/index.js
@@ -5,6 +5,20 @@
         const t = topics.find(c => c.classList.contains("selected"));
         if(t) {
             topicview.innerHTML = await (await fetch(t.data.body)).text();
+            const next = document.createElement("div"), back = document.createElement("div");
+            next.className = "next";
+            next.textContent = "Next";
+            
+            topicview.appendChild(next);
+            if(topics.indexOf(t) != -1 && !topics[topics.indexOf(t) + 1]) { next.style.setProperty("display","none") }
+            next.addEventListener("click",()=>{if(topics.indexOf(t)!=-1){if(topics[topics.indexOf(t)+1]){topics.map(x=>x.classList.remove("selected"))
+            topics[topics.indexOf(t)+1].click();topics[topics.indexOf(t)+1].classList.add("selected");refreshTopicView()}else{next.style.setProperty("display","none")}}else{next.style.setProperty("display","none")}})
+            
+            back.className="back";back.textContent="Back";topicview.appendChild(back);if(topics.indexOf(t)!=-1&&!topics[topics.indexOf(t)-1]){back.style.setProperty("display","none")}
+            back.addEventListener("click",()=>{if(topics.indexOf(t)!=-1){if(topics[topics.indexOf(t)-1]){topics.map(x=>x.classList.remove("selected"))
+            topics[topics.indexOf(t)-1].click();topics[topics.indexOf(t)-1].classList.add("selected");refreshTopicView()}else{back.style.setProperty("display","none")}}else{back.style.setProperty("display","none")}})
+
+            localStorage.setItem("selected", t.data.name)
         }
     };
 
@@ -17,7 +31,13 @@
         div.textContent = e.name;
         div.title = e.desc;
         div.className = "topic";
-        if(e.selected) div.classList.add("selected")
+        if(localStorage.getItem("selected") ) {
+            const sel = localStorage.getItem("selected");
+            e.name == sel ? div.classList.add("selected") : null
+        } else {
+            div.classList.add("selected");
+        }
+
         div.data = e;
 
         sidenav.appendChild(div);
